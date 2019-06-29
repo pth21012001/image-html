e5254393c513b52c0831a6d6b95e53a1f9adf44b
Your Name <you@example.com>
2019-06-30 02:44:24 +0700

    Them Phan Shop

---
+++ hieuungHeader.js
@@ -0,0 +1,37 @@
+function shop()
+{
+	var idPhan2 = document.getElementById("anh");
+	var idPhan3 = document.getElementById("slide2");
+	var idPhan4 = document.getElementById("content");
+	var idPhan5 = document.getElementById("sign");
+	var idPhan6 = document.getElementById("goBar");
+	// var idPhan7 = document.getElementById("footter");
+	var hienShop = document.getElementById("hienShop");
+
+	idPhan2.style.display = "none";
+	idPhan3.style.display = "none";
+	idPhan4.style.display = "none";
+	idPhan5.style.display = "none";
+	idPhan6.style.display = "none";
+	// idPhan7.style.display = "none";
+	hienShop.style.display = "block";
+}
+
+function goBar()
+{
+	var idPhan2 = document.getElementById("anh");
+	var idPhan3 = document.getElementById("slide2");
+	var idPhan4 = document.getElementById("content");
+	var idPhan5 = document.getElementById("sign");
+	var idPhan6 = document.getElementById("goBar");
+	// var idPhan7 = document.getElementById("footter");
+	var hienShop = document.getElementById("hienShop");
+
+	idPhan2.style.display = "block";
+	idPhan3.style.display = "block";
+	idPhan4.style.display = "block";
+	idPhan5.style.display = "block";
+	idPhan6.style.display = "block";
+	// idPhan7.style.display = "none";
+	hienShop.style.display = "none";
+}
