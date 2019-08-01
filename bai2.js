42ff21c782feb7947cea0ba2833cf2ff1395baf7
Your Name <you@example.com>
2019-08-01 22:54:40 +0700

    1/8/2019

--- bai2.js
+++ bai2.js
@@ -24,23 +24,7 @@
                 interval: 5000,
                 pauseOnHover: true
             },
-            responsiveBreakpoints: { 
-                portrait: { 
-                    changePoint:480,
-                    visibleItems: 1,
-                    itemsToScroll: 1
-                }, 
-                landscape: { 
-                    changePoint:640,
-                    visibleItems: 2,
-                    itemsToScroll: 2
-                },
-                tablet: { 
-                    changePoint:768,
-                    visibleItems: 3,
-                    itemsToScroll: 3
-                }
-            },
+            
             loaded: function(){ },
             before: function(){ },
             after: function(){ },
